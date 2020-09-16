9678cba58dc917affd7762f1f088e262296013cc
Jacob Shin <jacobshin313@gmail.com>
2020-09-16 18:32:08 -0400

    Add everything

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class DailyCase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(64), index=True, unique=True)
+    onCampusStudents = db.Column(db.Integer, index=True)
+    offCampusStudents = db.Column(db.Integer, index=True)
+    nonCampusStudents = db.Column(db.Integer, index=True)
+    employees = db.Column(db.Integer, index=True)
+    total = db.Column(db.Integer, index=True)
+
+    def __repr__(self):
+        return '<DailyCase {}>'.format(self.date)
